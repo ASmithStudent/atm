d151cd8c63c152a16ca7f4b047c99eb35bc5db9a
ASmithStudent <acsmith11@dons.usfca.edu>
2019-02-24 19:22:10 -0800

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,93 @@
+/**
+ * A Java class that allows a user to create an account that stores the type of account,
+ * the name of the person that owns the account, and the balance of the account.
+ * @author Adrian Smith
+ * @version 1.0
+ *
+ */
+
+public class Account {
+
+    int balance;
+    String name;
+    String type;
+
+    /**
+     * Constructor method that takes in account type, name of account, and initial balance.
+     *
+     */
+    public Account(String type, String name, int amount) {
+        this.type = type;
+        this.name = name;
+        this.balance = amount;
+    }
+
+    /**
+     * Adds amount to account's balance
+     */
+    void credit(int amount) {
+        this.balance += amount;
+    }
+
+    /**
+     * Subtracts amount from account balance, only if the account's new balance is not less than 0.
+     * Returns true if new balance is greater than 0, false if new account balance is less than 0.
+     *
+     */
+    boolean debit(int amount) {
+        int new_balance = this.balance - amount;
+
+        if (new_balance <= 0) {
+            return false;
+        } else {
+            this.balance = new_balance;
+            return true;
+        }
+    }
+
+    /**
+     * Returns account balance
+     */
+    int getBalance() {
+        return this.balance;
+    }
+
+    public String toString(String name, String type, int balance) {
+        return "Name: " + name + " Type: " + type + " Balance: " + balance;
+    }
+
+
+    public static void main(String[] args) {
+
+        Account account1 = new Account("checking", "Adrian", 1000);
+
+        account1.credit(500);
+        String account1Info = account1.toString(account1.name, account1.type, account1.balance);
+
+        System.out.print(account1Info);
+
+
+//        account1.credit(10);
+//        System.out.println("Account type: " + account1.type + " Account name: " + account1.name + " Account balance: " + account1.balance);
+//
+//        account1.debit(9);
+//        System.out.println("Account type: " + account1.type + " Account name: " + account1.name + " Account balance: " + account1.balance);
+//
+//        account1.getBalance();
+//        System.out.println("Account type: " + account1.type + " Account name: " + account1.name + " Account balance: " + account1.balance);
+//
+//        Account account2 = new Account("savings", "Jack", 5000);
+//
+//        account2.credit(100);
+//        System.out.println("Account type: " + account2.type + " Account name: " + account2.name + " Account balance: " + account2.balance);
+//
+//        account2.debit(1000);
+//        System.out.println("Account type: " + account2.type + " Account name: " + account2.name + " Account balance: " + account2.balance);
+//
+//        account2.getBalance();
+//        System.out.println("Account type: " + account2.type + " Account name: " + account2.name + " Account balance: " + account2.balance);
+
+
+    }
+
+}
---
+++ src/BankUser.java
@@ -0,0 +1,109 @@
+/**
+ * A Java class that defines a user at a Bank, tracking the user's id and pin
+ * that they use at an ATM machine and the accounts the user owns.
+ * @author Adrian Smith
+ * @version 1.0
+ *
+ */
+
+
+public class BankUser {
+
+    Account[] accounts;
+    String name;
+    String id;
+    String pin;
+
+
+    /**
+     * BankUser constructor method
+     */
+
+    public BankUser(String name, String id, String pin, Account[] accounts) {
+        this.name = name;
+        this.id = id;
+        this.pin = pin;
+        this.accounts = accounts;
+    }
+
+    /**
+     * Gets BankUser's accounts
+     * @return Account[]
+     */
+
+    Account[] getAccounts() {
+
+        return this.accounts;
+    }
+
+    /**
+     * Gets BankUser's name
+     * @return String
+     */
+
+    String getName() {
+        return this.name;
+    }
+
+    /**
+     * Converts BankUser info to string
+     * @return String
+     */
+
+    @Override
+    public String toString() {
+        return this.name + "[" + this.id + ":" + this.pin + "]";
+    }
+
+    /**
+     * Main method: Creates two users - one with 3 accounts and one with two accounts and then calls methods for each user
+     * @param args
+     * @return void
+     *
+     */
+
+    public static void main(String[] args) {
+
+        // First User
+        Account user1Account1 = new Account("Checking", "Main", 200);
+        Account user1Account2 = new Account("Savings", "Main Savings", 1000);
+        Account user1Account3 = new Account("Investment", "Main Investment", 5000);
+        Account[] user1Accounts = {user1Account1, user1Account2, user1Account3};
+
+        BankUser user1 = new BankUser("Adrian", "usf", "123456", user1Accounts );
+        Account[] userAccounts = user1.getAccounts();
+        for (Account account : userAccounts) {
+            System.out.println(account.toString(account.name, account.type, account.balance));
+        }
+
+        String user1Name = user1.getName();
+        System.out.println("User 1's name: " + user1Name);
+
+        String userInfo = user1.toString();
+        System.out.println("User 1's info: " + userInfo);
+
+
+        // Second User
+        Account user2Account1 = new Account("Savings", "Main", 10000);
+        Account user2Account2 = new Account("401k", "Main", 109000);
+
+
+        Account[] user2Accounts = {user2Account1, user2Account2};
+        BankUser user2 = new BankUser("Doug", "usf", "654321", user2Accounts);
+
+        Account[] user2Accs = user2.getAccounts();
+        for (Account account : user2Accs) {
+            System.out.println(account.toString(account.name, account.type, account.balance));
+        }
+
+        String user2Name = user2.getName();
+        System.out.println("User 2's name: " + user2Name);
+
+        String user2Info = user2.toString();
+        System.out.println("User 2's info: " + user2Info);
+
+    }
+
+
+
+}
